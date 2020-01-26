74393e57a440fd9843097ee35b5fe995511ee8e2
Black Berry <marat4800@gmail.com>
2020-01-26 22:36:50 +0200

    next

---
+++ target/classes/database/table_users.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `users_rest` . `users`(
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`name` VARCHAR(50) NOT NULL,
+`second_name` VARCHAR(100) NOT NULL,
+`payment` INT NOT NULL
+);
---
+++ target/classes/database/usersrest.sql
@@ -0,0 +1,1 @@
+INSERT INTO users VALUES (1,'Marat','Khusnutdinov',1000);
